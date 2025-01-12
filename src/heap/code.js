e8659b787d7f76a7c0647062d45086f53e0d0cf8
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-12 07:00:58 +0100

    hepaify

--- src/heap/code.js
+++ src/heap/code.js
@@ -27,24 +27,15 @@ class Heap {
   }
 
   bubbleUp(index) {
-    // while(index > 1) {
-    // 	const parentIndex = this.getParentIndex(index)
+    while (index > 1) {
+      const parentIndex = this.getParentIndex(index);
 
-    // 	if (this.heap[parentIndex] > this.heap[index]) {
-    // 		this.swap(index, parentIndex)
-    // 		index = parentIndex
-    // 	} else {
-    // 		break
-    // 	}
-    // }
-
-    if (index <= 1) return;
-
-    const parentIndex = this.getParentIndex(index);
-
-    if (this.heap[parentIndex] > this.heap[index]) {
-      this.swap(index, parentIndex);
-      return this.bubbleUp(parentIndex);
+      if (this.heap[parentIndex] > this.heap[index]) {
+        this.swap(index, parentIndex);
+        index = parentIndex;
+      } else {
+        break;
+      }
     }
 
     return;
@@ -57,59 +48,45 @@ class Heap {
     const res = this.heap[1];
     this.heap[1] = this.heap.pop();
 
-    let i = 1;
+    let index = 1;
 
-    // Do we have a left child still?
-    // We know a right child only exists if a left one does
-    // since complete binary tree
-    while (2 * i < this.heap.length) {
-      const leftChild = this.heap[this.getLeftChildIndex(i)];
+    this.bubbleDown(index);
+
+    return res;
+  }
+
+  heapify(arr) {
+    const heap = [null, ...arr];
+
+    const startIndex = Math.floor((arr.length - 1) / 2);
+
+    for (let i = startIndex; i > 0; i--) {
+      this.bubbleDown(i);
+    }
+
+    this.heap = heap;
+  }
+
+  bubbleDown(index) {
+    while (2 * index < this.heap.length) {
+      const leftChild = this.heap[this.getLeftChildIndex(index)];
       // could be undefined
-      const rightChild = this.heap[this.getRightChildIndex(i)];
-      const currentValue = this.heap[i];
+      const rightChild = this.heap[this.getRightChildIndex(index)];
+      const currentValue = this.heap[index];
 
       const shouldSwapRight =
         leftChild < currentValue && rightChild && leftChild < rightChild;
       const shouldSwapLeft = leftChild < currentValue;
 
       if (shouldSwapRight) {
-        this.swap(i, this.getRightChildIndex(i));
-        i = this.getRightChildIndex(i);
+        this.swap(index, this.getRightChildIndex(index));
+        index = this.getRightChildIndex(index);
       } else if (shouldSwapLeft) {
-        this.swap(i, this.getLeftChildIndex(i));
-        i = this.getLeftChildIndex(i);
+        this.swap(index, this.getLeftChildIndex(index));
+        index = this.getLeftChildIndex(index);
       } else {
         break;
       }
     }
-
-    // Just playing around how the recursive solution may look like
-    //     this.bubbleDown(i);
-
-    return res;
-  }
-
-  bubbleDown(index) {
-    if (2 * index < this.heap.length) return;
-
-    const leftChild = this.heap[this.getLeftChildIndex(index)];
-    const rightChild = this.heap[this.getRightChildIndex(index)];
-    const currentValue = this.heap[index];
-
-    const shouldSwapRight =
-      leftChild < currentValue && rightChild && leftChild < rightChild;
-    const shouldSwapLeft = leftChild < currentValue;
-
-    if (shouldSwapRight) {
-      this.swap(index, this.getRightChildIndex(i));
-      return this.bubbleDown(this.getRightChildIndex(i));
-    }
-
-    if (shouldSwapLeft) {
-      this.swap(index, this.getLeftChildIndex(i));
-      return this.bubbleDown(this.getLeftChildIndex(i));
-    }
-
-    return;
   }
 }
